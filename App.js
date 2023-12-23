875638ea917faed19fa9a85fb7db7e65b41317e0
zachvlat <zachariasvlatakis@gmail.com>
2023-12-23 17:35:25 +0200

    dark mode

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppBar from './components/AppBar'
 import ListView from './components/ListView';
 
 export default function App() {
   return (
-    <SafeAreaProvider style={styles.container}>
-      <StatusBar style="auto" />
-      <AppBar></AppBar>
-      <ListView></ListView>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="inverted" />
+        <AppBar />
+        <ListView />
+      </View>
     </SafeAreaProvider>
   );
 }
